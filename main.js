261bd977d38db1e3dda7b15242b8a8d0333c86d9
Vihaan15 <73476143+Vihaan15@users.noreply.github.com>
2021-07-05 17:45:57 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+song1 = "";
+song2 = "";
+
 function setup(){
     canvas = createCanvas(600,500);
     canvas.center();
@@ -9,3 +12,29 @@ function setup(){
     poseNet.on('pose' , gotPoses);
 
 }
+
+function preload() 
+{
+     song1 = loadSound("Harry Potter Theme Song.mp3"); 
+     song2 = loadSound("Dance Monkey mp3.mp3");
+ }
+
+ function draw() { 
+     image(video, 0, 0, 600, 500); 
+     song1_status = song1.isPlaying(); 
+     song2_status = song2.isPlaying();
+      fill("#FF0000"); 
+      stroke("#FF0000"); 
+      if(scoreRightWrist > 0.2)
+       { circle(rightWristX,rightWristY,20);
+        
+         song2.stop(); if(song1_status == false) 
+             
+         { song1.play(); document.getElementById("song").innerHTML = "Playing - Harry Potter Theme Song" }
+       }
+          if(scoreLeftWrist > 0.2) { circle(leftWristX,leftWristY,20);
+                                    song1.stop(); if(song2_status == false)
+             { song2.play();
+              document.getElementById("song").innerHTML = "Playing - Dance Monkey Song" } 
+                                   }
+ }
